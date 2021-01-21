fd1e012416b636604cd957e6bdb7b793863ec489
DonRyuDragoni <dragoni.ryu@gmail.com>
2021-01-21 18:22:27 -0300

    Load bootstrap ROM file from server

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-window.onload = () => {
+const BOOT_ROM = "DMG_ROM.bin";
+
+window.onload = async () => {
 	console.log("Hello world!");
 	const gameloc = document.getElementById("gamewindow");
 	console.log(gameloc);
+
+	const buffer = await fetch(BOOT_ROM);
+	const blob = await buffer.blob();
+	const abuff = await blob.arrayBuffer();
+	const arr = new Uint8Array(abuff);
+	console.log(arr);
 }
